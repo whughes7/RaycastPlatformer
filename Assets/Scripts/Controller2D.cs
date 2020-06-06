b2680828498c12c065450f598add8224e50e7568
will7hughes <will7hughes@gmail.com>
2020-06-06 16:25:19 -0500

    Note movement speed is 0.2f instead of 6

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent (typeof (BoxCollider2D))]
 public class Controller2D : MonoBehaviour
 {
+    // Used to determine which objects to collide with
+    public LayerMask collisionMask;
+
     const float skinWidth = 0.015f;
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
@@ -18,16 +21,86 @@ public class Controller2D : MonoBehaviour
     void Start()
     {
         collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
     }
 
-    void Update()
+    public void Move(Vector3 moveDistance)
     {
         UpdateRaycastOrigins();
-        CalculateRaySpacing();
+
+        if (moveDistance.x != 0)
+        {
+            HorizontalCollisions(ref moveDistance);
+        }
+        if (moveDistance.y != 0)
+        {
+            VerticalCollisions(ref moveDistance);
+        }
+
+        transform.Translate(moveDistance);
+    }
+
+    // Changes in this method effect moveDistance Move method
+    void HorizontalCollisions(ref Vector3 moveDistance)
+    {
+        float directionX = Mathf.Sign(moveDistance.x);
+        float rayLength = Mathf.Abs(moveDistance.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            // If moving down, cast from bottom left corner
+            // If moving up, cast from bottom right corner
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+
+            // Set x moveDistance to amount needed to move from current position to the point which the ray collided with obstacle
+            if (hit)
+            {
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red, 0.01f);
+
+                moveDistance.x = (hit.distance - skinWidth) * directionX;
+                // Set all ray lengths to the nearest hit ray
+                // Avoids clipping scenario
+                rayLength = hit.distance;
+            }
+            else
+            {
+                Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.green, 0.01f);
+            }
+        }
+    }
+
+
+    // Changes in this method effect moveDistance inside Move method
+    void VerticalCollisions(ref Vector3 moveDistance)
+    {
+        float directionY = Mathf.Sign(moveDistance.y);
+        float rayLength = Mathf.Abs(moveDistance.y) + skinWidth;
 
         for (int i = 0; i < verticalRayCount; i++)
         {
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red, 0.01f);
+            // If moving down, cast from bottom left corner
+            // If moving up, cast from top left corner
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + moveDistance.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+
+            // Set y moveDistance to amount needed to move from current position to the point which the ray collided with obstacle
+            if (hit)
+            {
+                Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red, 0.01f);
+
+                moveDistance.y = (hit.distance - skinWidth) * directionY;
+                // Set all ray lengths to the nearest hit ray
+                // Avoids clipping scenario
+                rayLength = hit.distance;
+            } else
+            {
+                Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.green, 0.01f);
+            }
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour
 {
+    float moveSpeed = 0.2f;
+    float gravity = -20;
+    Vector3 velocity;
+
     Controller2D controller;
 
     // Start is called before the first frame update
@@ -18,6 +22,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x += input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
