1e1857868ebfda2e33f95c77d1a9d93a17ce824e
will7hughes <will7hughes@gmail.com>
2020-06-06 15:16:19 -0500

    Initial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour
+{
+    Controller2D controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
