1759b3bb45ce3ff7e612dc7ddbc8d2b465a008e6
will7hughes <will7hughes@gmail.com>
2020-06-06 21:21:55 -0500

    NOTE: Bug where FPS will increase/decrease movement speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour
 {
-    // Assign values to these to determine gravity and jumpVelocity
-    float jumpHeight = 2f;
-    float timeToJumpApex = 0.4f;
+    [SerializeField] private float maxJumpHeight;
+    [SerializeField] private float timeToJumpApex;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float accelerationTimeAirborne;
+    [SerializeField] private float accelerationTimeGrounded;
 
-    // Determined by the above two variables
-    float jumpVelocity;
-    float gravity;
-
-    float accelerationTimeAirborne = 0.2f;
-    float accelerationTimeGrounded = 0.1f;
-
-    float moveSpeed = 6f;
     Vector3 velocity;
-    float velocityXSmoothing;
+    Vector3 prevVelocity;
+
+    private float maxHeightReached = Mathf.NegativeInfinity;
+    private float startHeight = Mathf.NegativeInfinity;
+    private float velocityXSmoothing;
+    private bool reachedApex = true;
+
+    float jumpForce;
+    float gravity;
+    private float gravityDown;
+
+    private float jumpTimer = 0;
 
     Controller2D controller;
 
@@ -32,11 +38,14 @@ public class Player : MonoBehaviour
     {
         controller = GetComponent<Controller2D>();
 
-        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        gravity = -2 * maxJumpHeight / Mathf.Pow(timeToJumpApex, 2);
+        gravityDown = gravity * 2;
+
+        //gravityMultiplier = 
+        jumpForce = 2 * maxJumpHeight / timeToJumpApex;
 
         gravityText.text = gravity.ToString();
-        jumpVelocityText.text = jumpVelocity.ToString();
+        jumpVelocityText.text = jumpForce.ToString();
     }
 
     // Update is called once per frame
@@ -47,7 +56,7 @@ public class Player : MonoBehaviour
         {
             velocity.y = 0;
         }
-        
+
         // Removes the continuous collision force left/right
         if (controller.collisions.left || controller.collisions.right)
         {
@@ -58,16 +67,51 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
         {
-            velocity.y = jumpVelocity;
+            Jump();
         }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("min gravity"); 
+            gravity = gravityDown;
+        }
+
+        if (!controller.collisions.below && !reachedApex)
+        {
+            jumpTimer += Time.fixedDeltaTime;
+        }
+
+        if (!reachedApex && maxHeightReached > transform.position.y)
+        {
+            float delta = maxHeightReached - startHeight;
+            float error = maxJumpHeight - delta;
+            Debug.Log("Jump Result: startHeight:" + Math.Round(startHeight, 4) + ", maxHeightReached:" + Math.Round(maxHeightReached, 4) + ", delta:" + Math.Round(delta,4) + ", error:" + Math.Round(error, 4) + ", jumpTimer:" + jumpTimer + ", gravity:" + gravity + ", jumpForce:" + jumpForce +"\n\n");
+            reachedApex = true;
+            gravity = gravityDown;
+        }
+
+        maxHeightReached = Mathf.Max(transform.position.y, maxHeightReached);
+
+        prevVelocity = velocity;
 
         float targetVelocityX = input.x * moveSpeed;
         velocity.x = Mathf.SmoothDamp(
-            velocity.x, 
+            velocity.x,
             targetVelocityX,
-            ref velocityXSmoothing, 
+            ref velocityXSmoothing,
             (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        velocity.y += gravity * Time.fixedDeltaTime;
+        Vector3 deltaPosition = (prevVelocity + velocity) * 0.5f * Time.fixedDeltaTime;
+        controller.Move(deltaPosition);
+    }
+
+    private void Jump()
+    {
+        jumpTimer = 0;
+        maxHeightReached = Mathf.NegativeInfinity;
+        gravity = -2 * maxJumpHeight / Mathf.Pow(timeToJumpApex, 2);
+        velocity.y = jumpForce;
+        startHeight = transform.position.y;
+        reachedApex = false;
     }
 }
